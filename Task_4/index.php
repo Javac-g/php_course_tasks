0c1cf67e0326718daf06aaf0d055d95d05bb31a7
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-08-28 21:39:33 +0300

    Update index.php

--- Task_4/index.php
+++ Task_4/index.php
@@ -2,43 +2,49 @@
 <html>
 <head>
 <style>
- body{
-   
-    
-   }
-   .even{ 
-   color:green;
-   }
-   .odd{ 
-   color:red;
-   }
-   .grid {
+  body {
+    /* Add any desired body styling here */
+  }
+
+  .even {
+    color: green;
+  }
+
+  .odd {
+    color: red;
+  }
+
+  .grid {
     display: grid;
     grid-template-columns: repeat(10, 1fr); /* 10 columns per row */
     grid-gap: 5px; /* Gap between cells */
   }
-   
 </style>
 </head>
 <body>
 <div class="grid">
-<table>
-<?php  
-for ($x = 2; $x <= 1000; $x++) {
+  <?php
+  for ($x = 1; $x <= 1000; $x++) {
+    echo "<table><tr>";
 
-	for($i = 1; $i <=1000; $i++){
-    	if($i == 1){
-        	
-  			echo " <tr class = 'odd'> <p class = 'odd'>$i </p></tr> ";
-            continue;
-		}
-		$class = $x % $i != 0 ? "odd":"even";
-  		echo " <tr class = '$class'> <p class = '$class'>$i </p></tr> ";
+    for ($i = 1; $i <= 10; $i++) {
+      $number = ($x - 1) * 10 + $i; // Calculate the current number
+      if ($number > 1000) {
+        break; // Stop when we exceed 1000
+      }
+
+      if ($i == 1) {
+        echo "<td class='odd'>$number</td>";
+        continue;
+      }
+
+      $class = $number % 2 == 0 ? "even" : "odd";
+      echo "<td class='$class'>$number</td>";
+    }
+
+    echo "</tr></table>";
   }
-}
-
-?>  
-</table>
+  ?>
 </div>
 </body>
 </html>
